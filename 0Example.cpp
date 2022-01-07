5cb5e43a76ab6ce83b28e03c4c08c8d5fa7f03a7
Rishabh <rishabhbisen2002@gmail.com>
2022-01-07 23:18:15 +0530

    dsa

---
+++ 0Example.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+     
+  
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){   // if we start from j=0 so array will reverse
+            if(arr[j]<arr[i]){
+            int temp;
+            temp =arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+    }
+    }
+
+    for(int i =0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
