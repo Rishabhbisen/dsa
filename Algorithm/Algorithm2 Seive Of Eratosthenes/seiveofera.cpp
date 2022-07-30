f5ad84d81f1dd3ba07865c7acfa0592a3d8a726a
Rishabh Bisen <79741674+Rishabhbisen@users.noreply.github.com>
2022-07-31 01:33:17 +0530

    Algorithm

---
+++ Algorithm/Algorithm2 Seive Of Eratosthenes/seiveofera.cpp
@@ -0,0 +1,30 @@
+//lecture 24
+#include<bits/stdc++.h>
+using namespace std;
+
+int  sieveOfEratosthenes(int n){
+    bool prime[n+1];
+    memset(prime,true,sizeof(prime));
+
+    int cnt = 0;
+    for(int i = 2; i < n; i++){
+        if(prime[i]){
+            cnt++;
+        }
+        for(int j = 2*i; j < n; j = j+i){
+            prime[j] = false;
+        }
+    }
+    return cnt;
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+    int ans = sieveOfEratosthenes(n);
+    cout<<ans;
+   
+
+    return 0;
+}
